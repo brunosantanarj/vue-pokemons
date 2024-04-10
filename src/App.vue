9b2197d1de0546ecdc3f81755edf4312bdcc71bd
Bruno <bruno@MacBodok-Pro-de-Bruno.local>
2024-04-10 14:09:54 -0300

    chore(base): base app configuration

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" />
+  </header>
+
+  <RouterView />
+</template>
